4130aedc4fa5d714dd4cac73bd5c1db67da77a0c
oTubby <thetubbyduck@gmail.com>
2024-03-23 12:53:24 -0700

    player changes

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 @export var speed = 150
-@export var sprite2d : Sprite2D
+@export var sprite2d : AnimatedSprite2D
+@export var raycast : RayCast2D
 const gravity = 500.0
 const WALK_SPEED = 150.0
 const JUMP_FORCE = 175.0
@@ -14,7 +15,10 @@ func get_input():
 	pass
 
 func attack():
-	pass
+	sprite2d.play("attack", 0.9)
+	if raycast.is_colliding():
+		pass
+	#print(sprite2d.get_playing_speed())
 
 func _process(delta):
 	get_input()
@@ -23,6 +27,7 @@ func _process(delta):
 	if Input.is_action_pressed("costumeSwitch") and not isBatmanMode:
 		sprite2d.set_frame(1)
 		isBatmanMode = true
+		
 	elif Input.is_action_pressed("costumeSwitchCivilian") and isBatmanMode:
 		sprite2d.set_frame(0)
 		isBatmanMode = false
@@ -33,12 +38,31 @@ func _process(delta):
 func _physics_process(delta):
 	velocity.y += delta * gravity
 	
+	
 	if Input.is_action_pressed("ui_left"):
+		sprite2d.flip_h = false
 		velocity.x = -WALK_SPEED
+		if not isBatmanMode:
+			sprite2d.play("Walk_Civ")
+		elif isBatmanMode:
+			sprite2d.play("Walk_Bat")
 	elif Input.is_action_pressed("ui_right"):
 		velocity.x =  WALK_SPEED
+		sprite2d.flip_h = true
+		if not isBatmanMode:
+			sprite2d.play("Walk_Civ")
+		elif isBatmanMode:
+			sprite2d.play("Walk_Bat")
 	else:
 		velocity.x = 0
+		if not isBatmanMode:
+			sprite2d.play("StandStill_Civ")
+		elif isBatmanMode:
+			#print(sprite2d.get_playing_speed())
+			if Input.is_action_pressed("attack") and isBatmanMode == true:
+				attack()
+			else:
+				sprite2d.play("StandStill_Bat")
 	if Input.is_action_pressed("ui_up") and is_on_floor():
 		velocity.y = -JUMP_FORCE
 	
