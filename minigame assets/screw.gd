f78ce940dacae5b0b06b49bee0a708ce82ccda2d
Uladog <nathan.gabrielson@gmail.com>
2024-03-23 11:21:32 -0700

    Stuff

--- minigame assets/screw.gd
+++ minigame assets/screw.gd
@@ -3,11 +3,12 @@ var rotationProg = 0
 var rotating = false
 var lockedIn = false
 @export var bar : ProgressBar
+@export var sprite : Sprite2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if rotating and !lockedIn:
-		rotation+=0.0349066
+		sprite.rotation+=0.0349066
 		rotationProg+=1
 		print(rotationProg)
 	if rotationProg >= 180:
@@ -16,8 +17,10 @@ func _process(delta):
 
 func _on_button_button_down():
 	rotating = true
+	bar.visible = true
 	pass # Replace with function body.
 
 func _on_button_button_up():
 	rotating = false
+	#bar.visible = false
 	pass # Replace with function body.
